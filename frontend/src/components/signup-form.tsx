1cda54ceec38acc776cc42ea14cd8f419ef28e2b
Ilham Muhammad <ilhamuh97@gmail.com>
2026-07-13 10:42:37 +0200

    remove sign up for OAuth

--- frontend/src/components/signup-form.tsx
+++ frontend/src/components/signup-form.tsx
@@ -37,9 +37,6 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
             <FieldGroup>
               <Field>
                 <Button type="submit">Create Account</Button>
-                <Button variant="outline" type="button">
-                  Sign up with Google
-                </Button>
                 <FieldDescription className="px-6 text-center">
                   Already have an account? <Link to="/login">Sign in</Link>
                 </FieldDescription>
